be802920b911ae42df60de031bc8eba12e101e01
madiha049 <44154901+madiha049@users.noreply.github.com>
2018-10-15 16:16:52 +0500

    Add files via upload

---
+++ assets/report.js
@@ -0,0 +1,15 @@
+function myFunc(){
+    var x = document.getElementById("togfunc");
+    if(x.style.display == "none")
+    x.style.display = "block";
+    else
+    x.style.display = "none";
+}
+
+function myFunc2(){
+    var x = document.getElementById("togfunc2");
+    if(x.style.display == "none")
+    x.style.display = "block";
+    else
+    x.style.display = "none";
+}
